25aea754a2f3141fc0f61348ca106cf99fca5902
Takea3 <38129465+Takea3@users.noreply.github.com>
2020-09-10 10:51:07 +0800

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -2,10 +2,11 @@
 cd /d %~dp0
 echo %cd%
 set cgong=完美，下一个。。。。 & set shibai=失败，再来一遍。。。。
-tasklist /nh|find /i "Appium.exe"
+set /a jjj=1
+tasklist /nh|find /i "node.exe"
 if errorlevel 1 (
     echo could not start while Appium not running
-    goto breakout
+    start appium
 ) else (
     REM echo Appium is running
 )
@@ -13,43 +14,36 @@ if errorlevel 1 (
 tasklist /nh|find /i "Nox.exe"
 if errorlevel 1 (
     echo could not start while Nox not running
-    goto breakout
+    start Nox.bat
+    TIMEOUT /T 25
 ) else (
     REM echo Nox is running
     goto run1
 )
 
 :run1
+set /a val=0
 setlocal enabledelayedexpansion
 for /f %%a in ('type %cd%\zhanghao.txt') do set /a val+=1
 echo !val!
-for /l %%j in (1,1,%val%) do (
-for /f "delims=" %%i in (%cd%\zhanghao.txt) do (
-set /a n+=1 & if !n!==%%j set "var=%%i"
-)
-set /a n=0
 
+for /l %%j in (!jjj!,1,!val!) do (
+for /f "delims=" %%i in (%cd%\zhanghao.txt) do (set /a n+=1 & if !n!==%%j set "var=%%i" & set "jj=%%j")
+set /a n=0
 :run
 echo !var!
 REM echo venv\scripts\python -m xuexi
 venv\scripts\python -m xuexi !var!
 TIMEOUT /T 10
-set SysTS=""
-set i=""
-for /f "delims=" %%i in ('powershell -c "Get-Date -UFormat '%%Y-%%m-%%d'"') do (set "SysTS=%%i")
-echo !SysTS!
-set b=""
-set str1=""
-for /f "delims=" %%b in (%cd%\logs\!SysTS!.log) do set "str1=%%~b"
-If NOT "!str1!"=="!str1:logout_or_not=!" (echo !cgong!) else echo !shibai! & goto run)
-set a=""
-set j=""
-set b=""
-set var=""
-set i=""
+for /f "delims=" %%c in ('powershell -c "Get-Date -UFormat '%%Y-%%m-%%d'"') do (set "shij=%%c")
 
-
+echo !shij!
+for /f "delims=" %%b in (%cd%\logs\!shij!.log) do set "str1=%%~b"
+echo !str1!
+If NOT "!str1!"=="!str1:logout_or_not=!" (echo %cgong%) else (echo %shibai% & goto run)
 )
+echo !jj!/!val!
+if !jj! lss !val! (set /a jjj=!jj!+1 & goto run1)
 pause
 exit
 
